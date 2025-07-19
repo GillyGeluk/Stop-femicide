9552c5f3ab1de357dc846db721cc9491e013cbb6
Gilly <haakmat92@outlook.com>
2025-07-19 18:57:30 +0200

    Create monthly_data.js

---
+++ data/monthly_data.js
@@ -0,0 +1,8 @@
+data/:const femicideData2024 = {
+  labels: ["Jan", "Feb", "Mrt", "Apr", "Mei", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"],
+  datasets: [{
+    label: "Femicidezaken in 2024",
+    data: [2, 1, 2, 3, 2, 1, 2, 2, 3, 2, 2, 2],
+    backgroundColor: "rgba(139,0,0,0.8)"
+  }]
+};
